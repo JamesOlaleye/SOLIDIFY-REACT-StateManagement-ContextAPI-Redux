f1b7ca4dfe18a8fa32fb207eb0d6a604a9ea7f82
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-03-13 10:45:38 +0100

    reusability with props improved

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
