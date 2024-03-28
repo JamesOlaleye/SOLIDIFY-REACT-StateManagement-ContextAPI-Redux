ff30275978c53833224d257632d07abd154b77ed
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-03-28 17:27:16 +0100

    Displaying questions

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -4,12 +4,13 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
-
   // 'loading', 'error','ready', 'active', 'finished'
   status: 'loading',
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -25,6 +26,12 @@ function reducer(state, action) {
         ...state,
         status: 'error',
       };
+
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+      };
     default:
       throw new Error('Action unknown');
   }
@@ -32,7 +39,10 @@ function reducer(state, action) {
 
 export default function App() {
   // const [state, dispatch] = useReducer(reducer, initialState);
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -50,7 +60,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   );
